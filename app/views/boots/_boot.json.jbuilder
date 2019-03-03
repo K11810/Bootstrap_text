e0dedd7aa8cba874b8281f462374ead6351cd3f0
K_18194 <fastofall6545@gmail.com>
2019-03-03 15:48:51 +0000

    blog features setting

---
+++ app/views/boots/_boot.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! boot, :id, :created_at, :updated_at
+json.url boot_url(boot, format: :json)
---
+++ db/migrate/20190303153605_create_boots.rb
@@ -0,0 +1,8 @@
+class CreateBoots < ActiveRecord::Migration[5.1]
+  def change
+    create_table :boots do |t|
+      t.text :content
+      t.timestamps
+    end
+  end
+end
